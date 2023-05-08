c432d73cb5811fe500e93aef2b72d5e1f50c5136
Eric Fortune <40925700+NJITFortune@users.noreply.github.com>
2023-05-08 08:17:38 -0400

    timeout and libcamera to takePhotos.sh

--- Waponi/bin/takePhotos.sh
+++ Waponi/bin/takePhotos.sh
@@ -6,38 +6,44 @@
 # Ensure that the panel lights are on for the photos
 ~/bin/PaneLightsOn.py
 
-# Take the photos for each camera
+echo "RPi1, Cameras start, `date`, `date +%s`" >> ~/rpi1.csv
+
+## Take the photos for each camera
+
+# Camera 0
 i2cset -y 10 0x24 0x24 0x02
 	cd ~/Pictures/Camera0a
-	~/bin/autoFocus.sh
+timeout 20 libcamera-still -n --autofocus-mode=auto --autofocus-range=macro --sharpness=2 --datetime --exposure=sport --quality=99
 	sleep 1
 	cd ~/Pictures/Camera0m
-	~/bin/manualFocus.sh
+timeout 20 libcamera-still -n --autofocus-mode=manual --lens-position=10 --sharpness=2 --datetime --exposure=sport --quality=99
 	sleep 1
 
+# Camera 1
 i2cset -y 10 0x24 0x24 0x12
 	cd ~/Pictures/Camera1a
-	~/bin/autoFocus.sh
+timeout 20 libcamera-still -n --autofocus-mode=auto --autofocus-range=macro --sharpness=2 --datetime --exposure=sport --quality=99
 	sleep 1
 	cd ~/Pictures/Camera1m
-	~/bin/manualFocus.sh
+timeout 20 libcamera-still -n --autofocus-mode=manual --lens-position=10 --sharpness=2 --datetime --exposure=sport --quality=99
 	sleep 1
 
+# Camera 2
 i2cset -y 10 0x24 0x24 0x22
 	cd ~/Pictures/Camera2a
-	~/bin/autoFocus.sh
+timeout 20 libcamera-still -n --autofocus-mode=auto --autofocus-range=macro --sharpness=2 --datetime --exposure=sport --quality=99
 	sleep 1
 	cd ~/Pictures/Camera2m
-	~/bin/manualFocus.sh
+timeout 20 libcamera-still -n --autofocus-mode=manual --lens-position=10 --sharpness=2 --datetime --exposure=sport --quality=99
 	sleep 1
 
+# Camera 3
 i2cset -y 10 0x24 0x24 0x32
 	cd ~/Pictures/Camera3a
-	~/bin/autoFocus.sh
+timeout 20 libcamera-still -n --autofocus-mode=auto --autofocus-range=macro --sharpness=2 --datetime --exposure=sport --quality=99
 	sleep 1
 	cd ~/Pictures/Camera3m
-	~/bin/manualFocus.sh
+timeout 20 libcamera-still -n --autofocus-mode=manual --lens-position=10 --sharpness=2 --datetime --exposure=sport --quality=99
 	sleep 1
 
-echo "RPi1, Cameras, `date`, `date +%s`" >> ~/rpi1.csv
-
+echo "RPi1, Cameras stop, `date`, `date +%s`" >> ~/rpi1.csv
