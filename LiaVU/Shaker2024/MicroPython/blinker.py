cde1c03ac4ce6cb38f231c9c3aea119620d1e759
Eric Fortune <40925700+NJITFortune@users.noreply.github.com>
2024-02-02 11:10:06 -0500

    Updated LiaVU

--- LiaVU/Shaker2024/MicroPython/blinker.py
+++ LiaVU/Shaker2024/MicroPython/blinker.py
@@ -2,11 +2,22 @@
 import machine
 import utime
 import MPU6050
+import uos
 
 # Set up the I2C interface
 i2c = machine.I2C(1, sda=machine.Pin(14), scl=machine.Pin(15))
 # Set up the MPU6050 class
 mpu = MPU6050.MPU6050(i2c)
+
+# This function counts the number of csv files in the root directory
+def count_csv_files():
+    file_list = uos.listdir("/")
+    csv_files = [file for file in file_list if file.endswith(".csv")]
+    return len(csv_files)
+
+# Get the number of CSV files in the root directory
+num_csv_files = count_csv_files()
+filename = "data{}.csv".format(num_csv_files)
 
 # wake up the MPU6050 from sleep
 mpu.wake()
@@ -33,7 +44,7 @@ utime.sleep_ms(500)
 led.on()
 
 # Open the data file for append and write header
-d = open("log.csv", "a")
+d = open(filename, "a")
 d.write("T, X, Y, Z \n")
 runtime = 0
 
